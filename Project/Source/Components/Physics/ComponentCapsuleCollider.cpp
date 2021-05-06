253137ffa0a8fce52f86f3db321d0e3439117b68
Isaac Fernandez Fontao <isaacfdez94@gmail.com>
2021-05-06 11:54:16 +0200

    Functionalities CapsuleComponent

--- Project/Source/Components/Physics/ComponentCapsuleCollider.cpp
+++ Project/Source/Components/Physics/ComponentCapsuleCollider.cpp
@@ -18,7 +18,9 @@ void ComponentCapsuleCollider::Init() {
 	if (!centerOffset.IsFinite()) {
 		ComponentBoundingBox* boundingBox = GetOwner().GetComponent<ComponentBoundingBox>();
 		if (boundingBox) {
-
+			radius = boundingBox->GetWorldOBB().HalfSize().MaxElement();
+			height = 2 * radius;
+			centerOffset = boundingBox->GetWorldOBB().CenterPoint() - GetOwner().GetComponent<ComponentTransform>()->GetGlobalPosition();
 		}
 	}
 	if (App->time->IsGameRunning() && !rigidBody) App->physics->CreateCapsuleRigidbody(this);
@@ -27,7 +29,7 @@ void ComponentCapsuleCollider::Init() {
 void ComponentCapsuleCollider::DrawGizmos() {
 	if (IsActiveInHierarchy()) {
 		ComponentTransform* ownerTransform = GetOwner().GetComponent<ComponentTransform>();	
-		
+		// TODO: Find how to draw a capsule.
 	}
 }
 
@@ -46,3 +48,66 @@ void ComponentCapsuleCollider::OnEditorUpdate() {
 	if (ImGui::DragFloat("Radius", &radius, App->editor->dragSpeed3f, 0.0f, inf) && App->time->IsGameRunning()) {
 		((btCapsuleShape*) rigidBody->getCollisionShape())->setImplicitShapeDimensions(btVector3(radius, height, 0));
 	}
+
+	if (ImGui::DragFloat("Height", &height, App->editor->dragSpeed3f, 0.0f, inf) && App->time->IsGameRunning()) {
+		((btCapsuleShape*) rigidBody->getCollisionShape())->setImplicitShapeDimensions(btVector3(radius, height, 0));
+	}
+
+	// TODO: Selector upAxis
+
+	if (ImGui::DragFloat3("Center Offset", centerOffset.ptr(), App->editor->dragSpeed2f, -inf, inf) && App->time->IsGameRunning()) {
+		float3 position = GetOwner().GetComponent<ComponentTransform>()->GetGlobalPosition();
+		Quat rotation = GetOwner().GetComponent<ComponentTransform>()->GetGlobalRotation();
+		rigidBody->setCenterOfMassTransform(btTransform(btQuaternion(rotation.x, rotation.y, rotation.z, rotation.w), btVector3(position.x, position.y, position.z)) * btTransform(btQuaternion::getIdentity(), btVector3(centerOffset.x, centerOffset.y, centerOffset.z)));
+	}
+
+	if (ImGui::Checkbox("Freeze rotation", &freezeRotation) && App->time->IsGameRunning()) {
+		motionState.freezeRotation = freezeRotation;
+	}
+}
+
+void ComponentCapsuleCollider::Save(JsonValue jComponent) const {
+	JsonValue jMass = jComponent[JSON_TAG_MASS];
+	jMass = mass;
+
+	JsonValue jRadius = jComponent[JSON_TAG_RADIUS];
+	jRadius = radius;
+
+	JsonValue jHeight = jComponent[JSON_TAG_HEIGHT];
+	jHeight = height;
+
+	JsonValue jCenterOffset = jComponent[JSON_TAG_CENTER_OFFSET];
+	jCenterOffset[0] = centerOffset.x;
+	jCenterOffset[1] = centerOffset.y;
+	jCenterOffset[2] = centerOffset.z;
+
+	JsonValue jFreeze = jComponent[JSON_TAG_FREEZE_ROTATION];
+	jFreeze = freezeRotation;
+
+	JsonValue jIsTrigger = jComponent[JSON_TAG_IS_TRIGGER];
+	jIsTrigger = isTrigger;
+}
+
+void ComponentCapsuleCollider::Load(JsonValue jComponent) {
+	JsonValue jMass = jComponent[JSON_TAG_MASS];
+	mass = jMass;
+
+	JsonValue jRadius = jComponent[JSON_TAG_RADIUS];
+	radius = jRadius;
+
+	JsonValue jHeight = jComponent[JSON_TAG_HEIGHT];
+	height = jHeight;
+
+	JsonValue jCenterOffset = jComponent[JSON_TAG_CENTER_OFFSET];
+	centerOffset = float3(jCenterOffset[0], jCenterOffset[1], jCenterOffset[2]);
+
+	JsonValue jFreeze = jComponent[JSON_TAG_FREEZE_ROTATION];
+	freezeRotation = jFreeze;
+
+	JsonValue jIsTrigger = jComponent[JSON_TAG_IS_TRIGGER];
+	isTrigger = jIsTrigger;
+}
+
+void ComponentCapsuleCollider::OnCollision() {
+	// TODO: Send event...
+}
