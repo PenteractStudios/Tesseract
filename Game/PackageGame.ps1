6a994c3672dcdf90737807b6fc236f4e1f68a598
Jaime Bea <jaimebeapz@gmail.com>
2021-05-18 00:26:41 +0200

    Hotfix PackageGame

--- Game/PackageGame.ps1
+++ Game/PackageGame.ps1
@@ -5,7 +5,8 @@ ${Version} = "0.1.0"
 
 ${PackageDir} = "Package"
 ${EngineSourceDir} = "Engine"
-${AssetsDir} = "Assets"
+${LibraryDir} = "Library"
+${FontsDir} = "Fonts"
 ${ReleaseDir} = "Penteract/Build/ReleaseEditor"
 ${LicensesDir} = "../Licenses"
 
@@ -16,8 +17,9 @@ New-Item -Path "${PackageDir}" -ItemType "Directory" -Force
 New-Item -Path "${PackageDir}/${LicensesDir}" -ItemType "Directory" -Force
 New-Item -Path "${PackageDir}/${LicensesDir}/${EngineName}" -ItemType "Directory" -Force
 Copy-Item -Path "${LicensesDir}" -Destination "${PackageDir}" -Force -Recurse
+Copy-Item -Path "${FontsDir}", "${LibraryDir}" -Destination "${PackageDir}" -Force -Recurse
 Copy-Item -Path "../LICENSE" -Destination "${PackageDir}/${LicensesDir}/${EngineName}" -Force -Recurse
 Copy-Item -Path "*.dll" -Destination "${PackageDir}" -Force -Recurse
 Copy-Item -Path "${ReleaseDir}/${GameProjectName}.dll" -Destination "${PackageDir}" -Force -Recurse
 Copy-Item -Path "${EngineSourceDir}/${EngineName}.exe" -Destination "${PackageDir}" -Force -Recurse
-Compress-Archive -Path "${PackageDir}/*" -DestinationPath "${OutDir}/${EngineName}_${Version}.zip" -Force
+Compress-Archive -Path "${PackageDir}/*" -DestinationPath "${OutDir}/${GameName}_${Version}.zip" -Force
