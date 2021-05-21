f4a056bc80ea98d678dce75fce82a5ba2ca60f5d
telmiuse <61976005+telmiuse@users.noreply.github.com>
2021-05-21 13:09:37 +0200

    Allow to shoot particles as u want

--- Project/Source/Components/ComponentParticleSystem.h
+++ Project/Source/Components/ComponentParticleSystem.h
@@ -68,6 +68,7 @@ private:
 	bool alphaTransparency = false; // Enables Alpha Transparency of the image and the color
 	bool isRandomFrame = false;
 	bool randomDirection = false;
+	bool shoot = false;
 	//TODO USE THIS FETURE
 	bool sizeOverTime = false;
 
