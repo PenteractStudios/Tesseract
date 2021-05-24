6ac733e6fc08702cbaa1cf5b32d1c60bcc0d40d2
magalenyo <magalenyo7@gmail.com>
2021-05-24 23:55:34 +0200

    Update Scene.h

--- Project/Source/Scene.h
+++ Project/Source/Scene.h
@@ -52,8 +52,8 @@ public:
 	void RemoveComponentByTypeAndId(ComponentType type, UID componentId);
 
 	int GetTotalTriangles() const;
-	std::vector<float> GetVertices();
-	std::vector<int> GetTriangles();
+	std::vector<float> GetVertices();		// Gets all the vertices from the MeshRenderer Components only if the ResourceMesh is found and the GameObject is Static
+	std::vector<int> GetTriangles();		// Gets all the triangles from the MeshRenderer Components only if the ResourceMesh is found and the GameObject is Static
 
 	void SetNavMesh(UID navMesh);
 	UID GetNavMesh();
