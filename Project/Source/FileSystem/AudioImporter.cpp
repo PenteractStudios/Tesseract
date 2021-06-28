574c7f0fc3434beabc0072581620a23720115fbd
Floren <magrinyafloren@gmail.com>
2021-06-28 19:02:41 +0200

    Encode Wav to OGG/Vorbis
    Encode Wavs to Ogg with LibsndFile

--- Project/Source/FileSystem/AudioImporter.cpp
+++ Project/Source/FileSystem/AudioImporter.cpp
@@ -13,12 +13,27 @@
 
 #include "Utils/Leaks.h"
 
+
+
 bool AudioImporter::ImportAudio(const char* filePath, JsonValue jMeta) {
 	LOG("Importing audio from path: \"%s\".", filePath);
 
 	// Timer to measure importing audio
 	MSTimer timer;
 	timer.Start();
+
+	//Convert Wavs to Ogg's
+	std::string extension = FileDialog::GetFileExtension(filePath);
+
+	if (extension == WAV_AUDIO_EXTENSION) {
+		std::string fileIn(filePath);
+		std::string fileOut = fileIn.replace(fileIn.end() - 3, fileIn.end(), "ogg");
+
+		char* filePathOgg;
+		filePathOgg = &fileOut[0];
+
+		EncondeWavToOgg(filePath, filePathOgg);
+	}
 
 	// Read from file
 	Buffer<char> buffer = App->files->Load(filePath);
@@ -51,4 +66,41 @@ bool AudioImporter::ImportAudio(const char* filePath, JsonValue jMeta) {
 	unsigned timeMs = timer.Stop();
 	LOG("Audio imported in %ums", timeMs);
 	return true;
+}
+
+void AudioImporter::EncondeWavToOgg(const char* infilename, const char* outfilename) {
+	static double data[BUFFER_LEN];
+
+	SNDFILE *infile, *outfile;
+	SF_INFO sfinfo;
+	int readcount;
+
+	if (!(infile = sf_open(infilename, SFM_READ, &sfinfo))) {
+		printf("Error : could not open file : %s\n", infilename);
+		puts(sf_strerror(NULL));
+		exit(1);
+	}
+
+	sfinfo.format = SF_FORMAT_OGG | SF_FORMAT_VORBIS;
+
+	if (!sf_format_check(&sfinfo)) {
+		sf_close(infile);
+		printf("Invalid encoding\n");
+		return;
+	};
+
+	if (!(outfile = sf_open(outfilename, SFM_WRITE, &sfinfo))) {
+		printf("Error : could not open file : %s\n", outfilename);
+		puts(sf_strerror(NULL));
+		exit(1);
+	};
+	
+	while ((readcount = sf_read_double(infile, data, BUFFER_LEN))) {
+		sf_write_double(outfile, data, readcount);
+	}
+
+	sf_close(infile);
+	sf_close(outfile);
+
+	return;
 }
--- Project/Source/FileSystem/AudioImporter.h
+++ Project/Source/FileSystem/AudioImporter.h
@@ -1,7 +1,16 @@
 #pragma once
 
 #include "FileSystem/JsonValue.h"
+//#include "Utils/oggHelper.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sndfile.h>
+
+#define BUFFER_LEN 4096
 
 namespace AudioImporter {
 	bool ImportAudio(const char* filePath, JsonValue jMeta);
+
+	static void EncondeWavToOgg(const char* infilename, const char* outfilename);
 };
