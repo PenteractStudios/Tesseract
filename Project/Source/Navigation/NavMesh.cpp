f7ceeba6d0b2e66168d96882dcd53b2540b52a4f
Jaime Bea <jaimebeapz@gmail.com>
2021-09-06 16:16:18 +0200

    Fix NavMesh memory leaks

--- Project/Source/Navigation/NavMesh.cpp
+++ Project/Source/Navigation/NavMesh.cpp
@@ -70,16 +70,16 @@ struct TileCacheData {
 };
 
 struct FastLZCompressor : public dtTileCacheCompressor {
-	virtual int maxCompressedSize(const int bufferSize) {
+	virtual int maxCompressedSize(const int bufferSize) override {
 		return (int) (bufferSize * 1.05f);
 	}
 
-	virtual dtStatus compress(const unsigned char* buffer, const int bufferSize, unsigned char* compressed, const int /*maxCompressedSize*/, int* compressedSize) {
+	virtual dtStatus compress(const unsigned char* buffer, const int bufferSize, unsigned char* compressed, const int /*maxCompressedSize*/, int* compressedSize) override {
 		*compressedSize = fastlz_compress((const void* const) buffer, bufferSize, compressed);
 		return DT_SUCCESS;
 	}
 
-	virtual dtStatus decompress(const unsigned char* compressed, const int compressedSize, unsigned char* buffer, const int maxBufferSize, int* bufferSize) {
+	virtual dtStatus decompress(const unsigned char* compressed, const int compressedSize, unsigned char* buffer, const int maxBufferSize, int* bufferSize) override {
 		*bufferSize = fastlz_decompress(compressed, compressedSize, buffer, maxBufferSize);
 		return *bufferSize < 0 ? DT_FAILURE : DT_SUCCESS;
 	}
@@ -99,7 +99,7 @@ struct LinearAllocator : public dtTileCacheAlloc {
 		resize(cap);
 	}
 
-	~LinearAllocator() {
+	virtual ~LinearAllocator() override {
 		dtFree(buffer);
 	}
 
@@ -109,12 +109,12 @@ struct LinearAllocator : public dtTileCacheAlloc {
 		capacity = cap;
 	}
 
-	virtual void reset() {
+	virtual void reset() override {
 		high = dtMax(high, top);
 		top = 0;
 	}
 
-	virtual void* alloc(const int size) {
+	virtual void* alloc(const size_t size) override {
 		if (!buffer)
 			return 0;
 		if (top + size > capacity)
@@ -124,7 +124,7 @@ struct LinearAllocator : public dtTileCacheAlloc {
 		return mem;
 	}
 
-	virtual void free(void* /*ptr*/) {
+	virtual void free(void* /*ptr*/) override {
 		// Empty
 	}
 };
@@ -140,7 +140,7 @@ struct MeshProcess : public dtTileCacheMeshProcess {
 		m_geom = geom;
 	}
 
-	virtual void process(struct dtNavMeshCreateParams* params, unsigned char* polyAreas, unsigned short* polyFlags) {
+	virtual void process(struct dtNavMeshCreateParams* params, unsigned char* polyAreas, unsigned short* polyFlags) override {
 		// Update poly flags from areas.
 		for (int i = 0; i < params->polyCount; ++i) {
 			if (polyAreas[i] == DT_TILECACHE_WALKABLE_AREA)
