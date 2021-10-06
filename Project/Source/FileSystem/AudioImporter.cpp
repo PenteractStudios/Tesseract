a5e0206fa29f1bd533cb8ce293b725b0670fc2c8
lowysole <lowy.sole@gmail.com>
2021-10-06 17:54:59 +0200

    Add Audio Import Options

--- Project/Source/FileSystem/AudioImporter.cpp
+++ Project/Source/FileSystem/AudioImporter.cpp
@@ -4,6 +4,7 @@
 #include "Application.h"
 #include "Modules/ModuleResources.h"
 #include "Modules/ModuleFiles.h"
+#include "Modules/ModuleEditor.h"
 #include "Modules/ModuleTime.h"
 #include "Resources/ResourceAudioClip.h"
 #include "Utils/Logging.h"
@@ -15,6 +16,42 @@
 
 #include "Utils/Leaks.h"
 
+#define JSON_TAG_IS_MONO "IsMono"
+#define JSON_TAG_AUDIO_FORMAT "AudioFormat"
+
+void AudioImportOptions::ShowImportOptions() {
+	ImGui::PushItemWidth(150);
+	ImGui::TextColored(App->editor->titleColor, "Audio Options");
+	ImGui::NewLine();
+	ImGui::Checkbox("Force To Mono", &isMono);
+	ImGui::NewLine();
+	const char* audioFormatItems[] = {"PCM (.wav)", "Vorbis (.ogg)"};
+	const char* currentAudioFormatItems = audioFormatItems[int(audioFormat)];
+	if (ImGui::BeginCombo("Compression Format", currentAudioFormatItems)) {
+		for (int n = 0; n < IM_ARRAYSIZE(audioFormatItems); ++n) {
+			bool is_selected = (currentAudioFormatItems == audioFormatItems[n]);
+			if (ImGui::Selectable(audioFormatItems[n], is_selected)) {
+				audioFormat = AudioFormat(n);
+			}
+			if (is_selected) {
+				ImGui::SetItemDefaultFocus();
+			}
+		}
+		ImGui::EndCombo();
+	}
+	ImGui::PopItemWidth();
+}
+
+void AudioImportOptions::Load(JsonValue jMeta) {
+	isMono = jMeta[JSON_TAG_IS_MONO];
+	audioFormat = (AudioFormat)(int) jMeta[JSON_TAG_AUDIO_FORMAT];
+}
+
+void AudioImportOptions::Save(JsonValue jMeta) {
+	jMeta[JSON_TAG_IS_MONO] = isMono;
+	jMeta[JSON_TAG_AUDIO_FORMAT] = (int) audioFormat;
+}
+
 bool AudioImporter::ImportAudio(const char* filePath, JsonValue jMeta) {
 	LOG("Importing audio from path: \"%s\".", filePath);
 
@@ -22,33 +59,40 @@ bool AudioImporter::ImportAudio(const char* filePath, JsonValue jMeta) {
 	MSTimer timer;
 	timer.Start();
 
-	// TODO: Fix Converter
-	////Convert Wavs to Ogg's
-	//std::string extension = FileDialog::GetFileExtension(filePath);
+	// Load import options
+	AudioImportOptions* importOptions = App->resources->GetImportOptions<AudioImportOptions>(filePath);
+	if (!importOptions) {
+		LOG("Failed to load import options.");
+		return false;
+	}
 
-	//if (extension == WAV_AUDIO_EXTENSION) {
-	//	std::string fileIn(filePath);
-	//	std::string fileOut = fileIn.replace(fileIn.end() - 4, fileIn.end(), ".ogg");
+	// Save import options to the meta file
+	importOptions->Save(jMeta);
 
-	//	char* filePathOgg;
-	//	filePathOgg = &fileOut[0];
+	// Convert & save audio
+	std::string fileName = FileDialog::GetFileNameAndExtension(filePath);
+	std::string fileNameTemp = "temp_" + fileName;
+	std::string fileIn(filePath);
+	const char* tempFilePath = fileIn.replace(fileIn.end() - fileName.length(), fileIn.end(), fileNameTemp).c_str();
 
-	//	EncondeWavToOgg(filePath, filePathOgg);
-	//}
-
-	// Read from file
-
-	EncondeStereoToMono(filePath);
-
-	Buffer<char> buffer = App->files->Load(filePath);
+	if (!ConvertAudio(filePath, tempFilePath, importOptions)) {
+		return false;
+	}
+	DEFER {
+		App->files->Erase(tempFilePath);
+	};
+	Buffer<char> buffer = App->files->Load(tempFilePath);
 	if (buffer.Size() == 0) {
-		LOG("Error loading audio %s", filePath);
+		LOG("Error loading audio %s", tempFilePath);
 		return false;
 	}
 
 	// Create audio clip resource
 	unsigned resourceIndex = 0;
 	std::unique_ptr<ResourceAudioClip> audioClip = ImporterCommon::CreateResource<ResourceAudioClip>(FileDialog::GetFileName(filePath).c_str(), filePath, jMeta, resourceIndex);
+
+	audioClip->isMono = importOptions->isMono;
+	audioClip->audioFormat = importOptions->audioFormat;
 
 	// Save resource meta file
 	bool saved = ImporterCommon::SaveResourceMetaFile(audioClip.get());
@@ -72,31 +116,41 @@ bool AudioImporter::ImportAudio(const char* filePath, JsonValue jMeta) {
 	return true;
 }
 
-void AudioImporter::EncondeWavToOgg(const char* infilename, const char* outfilename) {
+bool AudioImporter::ConvertAudio(const char* filePath, const char* convertedFilePath, const AudioImportOptions* audioImportOptions) {
 	static double data[BUFFER_LEN];
 
 	SNDFILE *inFile, *outFile;
 	SF_INFO sfInfo;
 	int readCount;
 
-	if (!(inFile = sf_open(infilename, SFM_READ, &sfInfo))) {
-		printf("Error : could not open file : %s\n", infilename);
-		puts(sf_strerror(NULL));
-		exit(1);
+	if (!(inFile = sf_open(filePath, SFM_READ, &sfInfo))) {
+		LOG("Error : could not open file %s", filePath);
+		sf_close(inFile);
+		return false;
 	}
 
-	sfInfo.format = SF_FORMAT_OGG | SF_FORMAT_VORBIS;
+	// Mono conversion
+	if (sfInfo.channels == 2 && audioImportOptions->isMono) {
+		sfInfo.channels = 1;
+		sfInfo.samplerate = sfInfo.samplerate * 2;
+	}
+
+	// Format conversion
+	if (audioImportOptions->audioFormat == AudioFormat::OGG) {
+		sfInfo.format = SF_FORMAT_OGG | SF_FORMAT_VORBIS;
+	} else {
+	}
 
 	if (!sf_format_check(&sfInfo)) {
+		LOG("Invalid encoding format");
 		sf_close(inFile);
-		printf("Invalid encoding\n");
-		return;
+		return false;
 	};
 
-	if (!(outFile = sf_open(outfilename, SFM_WRITE, &sfInfo))) {
-		printf("Error : could not open file : %s\n", outfilename);
-		puts(sf_strerror(NULL));
-		exit(1);
+	if (!(outFile = sf_open(convertedFilePath, SFM_WRITE, &sfInfo))) {
+		LOG("Error : could not open file %s", convertedFilePath);
+		sf_close(inFile);
+		return false;
 	};
 
 	while ((readCount = (int) sf_read_double(inFile, data, BUFFER_LEN))) {
@@ -106,56 +160,5 @@ void AudioImporter::EncondeWavToOgg(const char* infilename, const char* outfilen
 	sf_close(inFile);
 	sf_close(outFile);
 
-	return;
-}
-
-//Checks if a file is stereo and in that case makes a mono copy of it
-void AudioImporter::EncondeStereoToMono(const char* infilename) {
-	static double data[BUFFER_LEN];
-
-	SNDFILE *inFile, *outFile;
-	SF_INFO sfInfo;
-	int readCount;
-
-	if (!(inFile = sf_open(infilename, SFM_READ, &sfInfo))) {
-		printf("Error : could not open file : %s\n", infilename);
-		puts(sf_strerror(NULL));
-		exit(1);
-	}
-
-	if (sfInfo.channels == 2) {
-		sfInfo.channels = 1;
-		sfInfo.samplerate = sfInfo.samplerate * 2;
-
-		std::string fileName = FileDialog::GetFileNameAndExtension(infilename);
-		std::string fileNameMono = "mono_" + fileName;
-
-		std::string fileIn(infilename);
-		std::string fileOut = fileIn.replace(fileIn.end() - fileName.length(), fileIn.end(), fileNameMono);
-
-		char* outfilename;
-		outfilename = &fileOut[0];
-
-		if (!sf_format_check(&sfInfo)) {
-			sf_close(inFile);
-			printf("Invalid encoding\n");
-			return;
-		};
-
-		if (!(outFile = sf_open(outfilename, SFM_WRITE, &sfInfo))) {
-			printf("Error : could not open file : %s\n", outfilename);
-			puts(sf_strerror(NULL));
-			exit(1);
-		};
-
-		while ((readCount = (int) sf_read_double(inFile, data, BUFFER_LEN))) {
-			sf_write_double(outFile, data, readCount);
-		}
-
-		sf_close(inFile);
-		sf_close(outFile);
-
-		return;
-	}
-
+	return true;
 }
